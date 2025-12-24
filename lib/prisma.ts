5cca6ec26a39e1d784b3d5f278e13ed4ee20ca84
meangsun <sunfund88@gmail.com>
2025-12-24 15:19:27 +0700

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,18 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+// ใช้ชื่อตัวแปรที่แตกต่างเพื่อเลี่ยงความสับสน
+let prisma: PrismaClient
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['error'],
-  });
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  // @ts-ignore
+  if (!global.prisma) {
+    // @ts-ignore
+    global.prisma = new PrismaClient()
+  }
+  // @ts-ignore
+  prisma = global.prisma
 }
+
+export default prisma
