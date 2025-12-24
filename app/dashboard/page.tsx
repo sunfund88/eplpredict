122c58603b4cf5a61822e53ed77c240143bb2554
meangsun <sunfund88@gmail.com>
2025-12-24 19:15:11 +0700

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,14 @@
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-export default function Dashboard() {
-  const token = cookies().get('line_token')
-  if (!token) redirect('/')
+export default async function Dashboard() {
+  const cookieStore = await cookies()
+  const token = cookieStore.get('line_token')
 
-    
+  if (!token) {
+    redirect('/')
+  }
+
   return <h1>Dashboard</h1>
 }
+
