46fc6a42a5955ab10b57bf839ab783134dce067b
meangsun <sunfund88@gmail.com>
2025-12-25 15:17:10 +0700

    Update route.ts

--- app/logout/route.ts
+++ app/logout/route.ts
@@ -1,11 +1,13 @@
 import { NextResponse } from 'next/server'
 
-export async function POST(req: Request) {
+export async function GET(req: Request) {
+  // สร้าง Response ให้ Redirect ไปที่หน้า Home ('/')
   const res = NextResponse.redirect(new URL('/', req.url))
 
+  // ลบ Cookie โดยตั้งค่า maxAge เป็น 0
   res.cookies.set('user_session', '', {
     path: '/',
-    maxAge: 0, // ลบ cookie
+    maxAge: 0,
   })
 
   return res
