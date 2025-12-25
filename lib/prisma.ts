ff7f13aeae3363d673267cdfc2baa3d2d8e61736
meangsun <sunfund88@gmail.com>
2025-12-25 14:05:21 +0700

    db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,7 @@
 // @ts-ignore
 import { PrismaClient } from '@prisma/client'
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+console.log("DIRECT_URL:", process.env.DIRECT_URL);
 
 let prisma: any
 
