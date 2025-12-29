60f237fa035de77b7e041e2482490cad8f96ae10
meangsun <sunfund88@gmail.com>
2025-12-29 17:11:47 +0700

    UserDetail

---
+++ app/actions/user.ts
@@ -0,0 +1,15 @@
+// app/actions/user.ts
+'use server'
+import prisma from "@/lib/prisma"
+
+export async function getUserDetail(lineId: string) {
+  return await prisma.user.findUnique({
+    where: { id: lineId }, // สมมติว่า id ใน DB ของคุณคือ lineId
+    include: {
+      predictions: {
+        orderBy: { gw: 'desc' },
+        take: 20 // ดึงประวัติการทายผลล่าสุด 10 นัด
+      }
+    }
+  })
+}
--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
       
       user = await prisma.user.findUnique({
         where: { id: userId },
-        select: { id: true, name: true, image: true, score: true }
+        select: { id: true, lineId:true, name: true, image: true, score: true }
       })
     } catch (err) {
       console.error("JWT Invalid:", err)
@@ -72,7 +72,7 @@ export default async function Home() {
           </div>
 
           {/* รูป Profile (ขอบมนแบบในรูป Sketch) */}
-          <Link href="/userdetail" className="flex-shrink-0">
+          <Link href={`/userdetail/${user.id}`} className="flex-shrink-0">
             <div className="w-10 h-10 rounded-xl border-2 border-white/50 overflow-hidden bg-white/20">
               <img 
                 src={user.image!} 
